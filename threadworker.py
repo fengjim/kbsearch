aa7b512ec7e9b8591ab0836cd669556bb447f8da
Jim Feng <jinhua.feng.jim@gmail.com>
2018-03-17 08:33:50 +0000

    Add hyperlink to make Excel usage easier

--- threadworker.py
+++ threadworker.py
@@ -7,7 +7,8 @@ from selenium import webdriver
 
 # create logger
 logging.basicConfig(level=logging.INFO)
-kbformat='{updatetype:10}; {kburl}; {cpdate}; {kbdate}; {author}; {detail}'
+kbformat = '{updatetype:10}; {kburl}; {cpdate}; {kbdate}; {author}; {detail}'
+hyperlinkformat = '=HYPERLINK("{url}")'
 waiting_time = 10
 dateformat = '%Y-%m-%d'
 KB_VENDOR_VMWARE = 'VMware'
@@ -30,13 +31,16 @@ class ThreadWorker(threading.Thread):
     def __del__(self):
         self.driver.close()
 
+    def __hyperlink(self, url):
+        return hyperlinkformat.format(url = url)
+
     def __checkb(self, url, cpupdate, author):
         cpdate = date.today()
         try:
             cpdate = parse(cpupdate)
         except Exception as ex:
             print kbformat.format(updatetype='date_error',
-                kburl=url,
+                kburl=self.__hyperlink(url),
                 cpdate=cpupdate,
                 kbdate='unknown',
                 author=author,
@@ -50,28 +54,28 @@ class ThreadWorker(threading.Thread):
                 kbdate = parse(matches[0].text)
                 if kbdate > cpdate:
                     print kbformat.format(updatetype='update',
-                        kburl=url,
+                        kburl=self.__hyperlink(url),
                         cpdate=cpdate.strftime(dateformat),
                         kbdate=kbdate.strftime(dateformat),
                         author=author,
                         detail='')
                 elif kbdate < cpdate:
                     print kbformat.format(updatetype='data_lost',
-                        kburl=url,
+                        kburl=self.__hyperlink(url),
                         cpdate=cpdate.strftime(dateformat),
                         kbdate=kbdate.strftime(dateformat),
                         author=author,
                         detail='')
             except Exception as ex:
                 print kbformat.format(updatetype='date_error',
-                    kburl=url,
+                    kburl=self.__hyperlink(url),
                     cpdate=cpdate.strftime(dateformat),
                     kbdate='unknow',
                     author=author,
                     detail=ex.args)
         else :
             print kbformat.format(updatetype='page_error',
-                kburl=url,
+                kburl=self.__hyperlink(url),
                 cpdate=cpdate.strftime(dateformat),
                 kbdate='unknow',
                 author=author,
